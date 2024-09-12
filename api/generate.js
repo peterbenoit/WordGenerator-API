e4971180f1825acd0e51b46704c1d74980bb2c60
Pete Benoit <peterbenoit@gmail.com>
2024-09-12 19:37:13 -0400

    Update generate.js
    CORS

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,11 @@
 import { WordGenerator } from '../lib/WordGenerator.js';
 
 export default function handler(req, res) {
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   const { wordList = 'latin', format = 'string', limit = 10 } = req.query;
 
   // Initialize WordGenerator with the requested options
